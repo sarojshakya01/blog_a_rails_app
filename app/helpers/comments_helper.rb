40385b3e3dc1143eb9e9d2e647ead7f2e22e52cf
sarojshakya01 <sarojsh01@gmail.com>
2020-06-05 17:42:45 +0545

    minor css enhancement

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,11 +1,2 @@
 module CommentsHelper
-  def get_username(user_id)
-    # User.find(user_id).username
-    # temp soln for username
-    email = User.find(user_id).email
-    to = email.index("@")
-    username = email[0,to]
-    return username
-  end
-
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,13 @@
 module UsersHelper
+  def get_username(user_id)
+    # User.find(user_id).username
+    # temp soln for username
+    email = User.find(user_id).email
+    to = email.index("@")
+    username = email[0, to]
+    return username
+  end
+
   def get_num_of_posts(user_id)
     Post.where(:user_id => user_id).count
   end
